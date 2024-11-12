64a366cb28a31d0dc82803e4869ca0ea608bf4fd
Brandon La <77310351+la-brandon@users.noreply.github.com>
2024-11-12 16:38:19 -0500

    Delete BiddingBlitz-brandon directory

--- BiddingBlitz-brandon/src/main/java/com/example/BiddingBlitz/repository/auction/ForwardAuctionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BiddingBlitz.repository.auction;
-
-import com.example.BiddingBlitz.model.auction.ForwardAuction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ForwardAuctionRepository extends JpaRepository<ForwardAuction, Long> {
-}
--- BiddingBlitz-brandon/src/main/resources/templates/catalog.jsp
+++
@@ -1,9 +0,0 @@
-<h1>Catalog</h1>
-<ul>
-    <c:forEach var="item" items="${items}">
-        <li>
-            ${item.name} - ${item.startingBid}
-            <a href="/bid?itemId=${item.id}">Place Bid</a>
-        </li>
-    </c:forEach>
-</ul>
