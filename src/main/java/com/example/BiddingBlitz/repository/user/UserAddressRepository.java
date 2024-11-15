fa56cad22cb08f6e49835696f291744a402aee0d
Brandon La <77310351+la-brandon@users.noreply.github.com>
2024-11-15 12:56:59 -0500

    Delete src directory

--- src/main/java/com/example/BiddingBlitz/repository/user/UserAddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BiddingBlitz.repository.user;
-
-import com.example.BiddingBlitz.model.user.UserAddress;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAddressRepository extends JpaRepository<UserAddress, Long> {
-}
